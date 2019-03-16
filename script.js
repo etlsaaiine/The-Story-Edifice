ca652930d4d9e807d56220f20ad77025216b685b
etlsaaiine <43707303+etlsaaiine@users.noreply.github.com>
2019-03-16 11:34:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,7 @@ function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
   // usernames & passwords
-if ((username=='ellabc23'&& password=='lucaslove143')||
-    (username=='flowerjasmine' && password=='YASSister')||
-    (username=='thesecretlifeofpets' && password=='2')||
-    (username=='thedailykaylee'&& password=='frodopup')||
-    (username=='Minecrafter'&& password=='sleep')||
-    (username=='guest1'&& password=='adventureguest')||
-    (username=='guest2'&& password=='dragonguest')||
-    (username=='guest3'&& password=='thethirdguest')||
-    (username=='guest'&& password=='guest')||
-    (username=='HPlover' && password=='HermioneGranger')||
-    (username=='Unicorn' && password=='Mermaid')){
-  
+if ((username=='ellabc23'&& password=='lucaslove143')||(username=='flowerjasmine' && password=='YASSister')||(username=='thesecretlifeofpets' && password=='2')||(username=='thedailykaylee'&& password=='frodopup')||(username=='Minecrafter'&& password=='sleep')||(username=='guest1'&& password=='adventureguest')||(username=='guest2'&& password=='dragonguest')||(username=='guest3'&& password=='thethirdguest')||(username=='guest'&& password=='guest')||(username=='HPlover' && password=='HermioneGranger')||(username=='Unicorn' && password=='Mermaid')){
 window.location.href='home.html';
 }
 } 
